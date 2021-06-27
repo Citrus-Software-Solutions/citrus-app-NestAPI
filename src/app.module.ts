711f9e6fecdd0203cee0ba0e30ebfb47e39ca7b9
César <cesar_guzman2000@hotmail.com>
2021-06-27 15:35:40 -0400

    Initialize nest.js project

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { Configuration } from './config/config.enum';
+import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
+import { DatabaseModule } from './database/database.module';
+import { JobOffersModule } from './job-offers/job-offers.module';
+
+@Module({
+  imports: [ConfigModule, DatabaseModule, JobOffersModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {
+  static port: number | string;
+
+  constructor(private readonly _confiService: ConfigService) {
+    AppModule.port = this._confiService.get(Configuration.PORT);
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('apiOffers');
+  await app.listen(AppModule.port);
+}
+bootstrap();
