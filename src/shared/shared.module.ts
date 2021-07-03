9c5fa56e17626cc59fed304bd27338f25e2a2287
César <cesar_guzman2000@hotmail.com>
2021-07-03 09:37:42 -0400

    [feat] create mappers to employer and job offer

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { EmployerDataMapper } from './mappers/employer/employer.mapper';
+import { JobOfferDataMapper } from './mappers/job-offers/job-offers.mapper';
 
 @Module({
-  exports: [],
-  providers: [],
+  exports: [EmployerDataMapper, JobOfferDataMapper],
+  providers: [EmployerDataMapper, JobOfferDataMapper],
+  imports: [EmployerDataMapper],
 })
 export class SharedModule {}
