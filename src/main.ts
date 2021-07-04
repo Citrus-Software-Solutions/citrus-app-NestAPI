8ade8501415aa815aed2efb0c5089f918c32286b
Yorfrank Bastidas <ykbastidasc@gmail.com>
2021-07-04 06:31:43 -0400

    [ref] change global prefix after declare consts

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,10 @@ import { generateTypeormConfigFile } from './scripts';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api-nest');
   const logger = new Logger('Bootstrap');
   const config = app.get(ConfigService);
   const port = parseInt(config.get<string>(SERVER_PORT), 10) || 5432;
+  app.setGlobalPrefix('api-nest');
   // initSwagger(app);
   generateTypeormConfigFile(config);
   app.useGlobalPipes(
