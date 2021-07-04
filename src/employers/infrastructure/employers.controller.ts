45b216aaba37a96894990fd1bb1c6be1771b0f2e
César <cesar_guzman2000@hotmail.com>
2021-07-04 03:13:48 -0400

    [ref] clean code and delete unuse files

--- src/employers/infrastructure/employers.controller.ts
+++ src/employers/infrastructure/employers.controller.ts
@@ -1,16 +1,4 @@
-import {
-  //   Body,
-  Controller,
-  //   Delete,
-  Get,
-  HttpStatus,
-  Inject,
-  Res,
-  //   Param,
-  //   Patch,
-  //   Post,
-} from '@nestjs/common';
-import { Employer } from '../domain/employer.model';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { IEmployersService } from '../application/employers.service.interface';
 import { ReadEmployerDto } from '../dtos/read-employer.dto';
 
@@ -21,41 +9,8 @@ export class EmployersController {
     private readonly _employersService: IEmployersService,
   ) {}
 
-  //   @Post()
-  //   addEmployer(
-  //     @Body('name') companyName: string,
-  //     @Body('status') status: number,
-  //   ): any {
-  //     this.employersService.createEmployer(companyName, status);
-  //   }
-
   @Get()
   getAllEmployers(): Promise<ReadEmployerDto[]> {
     return this._employersService.getEmployers();
   }
-
-  //   @Get(':name')
-  //   getEmployer(@Param('name') companyName: string) {
-  //     return this.employersService.getSingleEmployer(companyName);
-  //   }
-
-  //   @Patch(':name')
-  //   updateEmployer(
-  //     @Param('name') actualCompanyName: string,
-  //     @Body('name') newCompanyName: string,
-  //     @Body('status') status: number,
-  //   ) {
-  //     this.employersService.updateEmployer(
-  //       actualCompanyName,
-  //       newCompanyName,
-  //       status,
-  //     );
-  //     return null;
-  //   }
-
-  //   @Delete(':name')
-  //   removeEmployer(@Param('name') companyName: string) {
-  //     this.employersService.deleteEmployer(companyName);
-  //     return null;
-  //   }
 }
