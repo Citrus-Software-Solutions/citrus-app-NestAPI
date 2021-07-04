06195a12733c669b951c3a00c8f8fd78c8126dcb
César <cesar_guzman2000@hotmail.com>
2021-07-04 02:55:34 -0400

    [feat] create dtos to show data to the client

---
+++ src/job-offers/dtos/read-job-offert.dto.ts
@@ -0,0 +1,66 @@
+import { Exclude, Expose, Type } from 'class-transformer';
+import {
+  IsDate,
+  IsDefined,
+  IsNotEmptyObject,
+  IsNumber,
+  IsObject,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import { ReadEmployerDto } from 'src/employers/dtos/read-employer.dto';
+
+@Exclude()
+export class ReadJobOffertDto {
+  @Expose()
+  @IsNumber()
+  readonly id: number;
+
+  @Expose()
+  @IsString()
+  readonly name: string;
+
+  @Expose()
+  @IsString()
+  readonly description: string;
+
+  @Expose()
+  @IsNumber()
+  readonly availableVacans: number;
+
+  @Expose()
+  @IsDate()
+  readonly dateBegin: Date;
+
+  @Expose()
+  @IsDate()
+  readonly dateEnd: Date;
+
+  @Expose()
+  @IsString()
+  readonly status: string;
+
+  @Expose()
+  @IsString()
+  readonly gender: string;
+
+  @Expose()
+  @IsNumber()
+  readonly salary: number;
+
+  @Expose()
+  @IsNumber()
+  readonly minAge?: number;
+
+  @Expose()
+  @IsNumber()
+  readonly maxAge?: number;
+
+  @Expose()
+  @IsDefined()
+  @IsNotEmptyObject()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => ReadEmployerDto)
+  readonly creador: ReadEmployerDto;
+}
