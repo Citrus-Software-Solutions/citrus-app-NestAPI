8120dd499a2e9d26092f5103111d30a68c2f5293
Yorfrank Bastidas <ykbastidasc@gmail.com>
2021-07-04 09:23:49 -0400

    [fix] Listen to dynamic port to deploy heroku

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,13 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 // import { initSwagger } from './app.swagger';
-import { SERVER_PORT } from './config/constants';
 import { generateTypeormConfigFile } from './scripts';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('Bootstrap');
   const config = app.get(ConfigService);
-  const port = parseInt(config.get<string>(SERVER_PORT), 10) || 5432;
+  const port = process.env.PORT || 5432;
   app.setGlobalPrefix('api-nest');
   // initSwagger(app);
   generateTypeormConfigFile(config);
