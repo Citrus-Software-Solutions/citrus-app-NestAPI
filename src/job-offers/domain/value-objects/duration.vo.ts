9b88053d8b45f23987e8c2d991f66707a0b6fc55
César <cesar_guzman2000@hotmail.com>
2021-07-17 21:03:04 -0400

    [ref] create job offer value objects

---
+++ src/job-offers/domain/value-objects/duration.vo.ts
@@ -0,0 +1,26 @@
+import { ValueObject } from '../../../shared/domain/value-object.abstract';
+
+interface DurationProps {
+  value: number;
+}
+
+export class Duration extends ValueObject<DurationProps> {
+  private constructor(props: DurationProps) {
+    super(props);
+  }
+
+  get value(): number {
+    return this.props.value;
+  }
+
+  public static create(hours: number): Duration {
+    if (hours === undefined || hours === null) {
+      throw new Error('Total of hours can not be empty.');
+    }
+    if (hours <= 0) {
+      throw new Error('Total of hours can not be zero or negative.');
+    } else {
+      return new Duration({ value: hours });
+    }
+  }
+}
---
+++ src/job-offers/domain/value-objects/money.vo.ts
@@ -0,0 +1,28 @@
+import { ValueObject } from '../../../shared/domain/value-object.abstract';
+
+interface MoneyProps {
+  value: number;
+}
+
+export class Money extends ValueObject<MoneyProps> {
+  private constructor(props: MoneyProps) {
+    super(props);
+  }
+
+  get value(): number {
+    return this.props.value;
+  }
+
+  public static create(amount: number): Money {
+    if (amount === undefined || amount === null) {
+      throw new Error('Payment for every hour of work can not be empty.');
+    }
+    if (amount <= 0) {
+      throw new Error(
+        'Payment for every hour of work can not be zero or negative.',
+      );
+    } else {
+      return new Money({ value: amount });
+    }
+  }
+}
