64ae1d3bd30c2049e979965662c24015699d5782
César <cesar_guzman2000@hotmail.com>
2021-07-21 22:12:52 -0400

    [ref] add contactinformation as part of new domain

---
+++ src/contact-information/dto/read-contactinformation.dto.ts
@@ -0,0 +1,25 @@
+import { Exclude, Expose, Transform } from 'class-transformer';
+import { IsNumber, IsString } from 'class-validator';
+import { Name } from '../../shared/domain/name.vo';
+import { JobTitle } from '../domain/value-objects/job-title.vo';
+
+@Exclude()
+export class ReadContactInformationDto {
+  @Expose()
+  @IsString()
+  @Transform(({ value }) => value.props.value)
+  readonly full_name: Name;
+
+  @Expose()
+  @IsString()
+  @Transform(({ value }) => value.props.value)
+  readonly job_title: JobTitle;
+
+  @Expose()
+  @IsNumber()
+  readonly phone_number: number;
+
+  @Expose()
+  @IsString()
+  readonly email: string;
+}
