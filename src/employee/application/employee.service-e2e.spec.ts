b318ef05fe65c30a526f63c66ad770b28fc479d5
Yorfrank Bastidas <ykbastidasc@gmail.com>
2021-08-04 12:19:31 -0400

    [ref] Delete unused mock of repository

--- src/employee/application/employee.service-e2e.spec.ts
+++ src/employee/application/employee.service-e2e.spec.ts
@@ -3,30 +3,14 @@ import { Test } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { EmployeeModule } from '../employee.module';
 import { EmployeeService } from './employee.service';
-import { EmployeeRepository } from './employee.repository';
 
-describe('Cats', () => {
+describe('Get all employees', () => {
   let app: INestApplication;
   const employeeService = { getEmployee: () => ['test'] };
-  const mockEmployeeRepository = {
-    getEmployee() {
-      return {
-        test: 'success',
-      };
-    },
-  };
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [EmployeeModule],
-      providers: [
-        {
-          provide: EmployeeRepository,
-          useValue: {
-            get: jest.fn(() => mockEmployeeRepository), // really it can be anything, but the closer to your actual logic the better
-          },
-        },
-      ],
     })
       .overrideProvider(EmployeeService)
       .useValue(employeeService)
