05d7dfc0bf63cc0ac6f8fd8e14cb72796e62a557
alejjb <ajjauregui.14@est.ucab.edu.ve>
2021-08-05 04:09:59 -0400

    [test] implement getEmployeeById unit test

--- src/employee/infrastructure/employee.controller.spec.ts
+++ src/employee/infrastructure/employee.controller.spec.ts
@@ -50,6 +50,47 @@ describe('EmployeeController', () => {
         },
       ];
     }),
+    getEmployeeById: jest.fn().mockImplementation((id: number) => ({
+      id,
+      first_name: 'Pedro',
+      middle_name: 'Antonio',
+      last_name: 'Ramirez',
+      phone_number: '(+58) 412 6672829',
+      birth_date: '1983-07-21',
+      address: {
+        street1: 'Calle 2',
+        street2: 'Calle alterna 2',
+        city: 'Ciudad 1',
+        state: 'State 1',
+        _zip: 'ZIP11',
+      },
+      ssn: '345678223',
+      education_level: 3,
+      work_experiences: [
+        {
+          job_title: 'Job title 1',
+          company_name: 'Company 3',
+          category: 1,
+        },
+      ],
+      skills: [
+        {
+          name: 'Skill 1',
+          category: 1,
+        },
+      ],
+      references: [
+        {
+          full_name: 'Person 3',
+          job_title: 'Job Title 3',
+          company_name: 'Company 3',
+          phone_number: 2211271,
+          email: 'email3@domain.com',
+        },
+      ],
+      rating: 4.3,
+      status: 2,
+    })),
   };
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -74,6 +115,17 @@ describe('EmployeeController', () => {
         .mockImplementation(() => result);
 
       expect(await employeeController.getAllEmployee()).toBe(result);
+    });
+  });
+  describe('Get one employee (unit)', () => {
+    it('should return an employee', async () => {
+      const result = {};
+
+      jest
+        .spyOn(employeeService, 'getEmployeeById')
+        .mockImplementation(() => result);
+
+      expect(await employeeController.getById(1)).toBe(result);
     });
   });
 });
